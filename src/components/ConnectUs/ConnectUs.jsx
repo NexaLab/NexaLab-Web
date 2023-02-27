34f79ba18b2b6077957eb894a752aa3c75b4c486
Abdur Rafay <abdulrafey055@gmail.com>
2023-02-28 02:19:37 +0500

    Connet-Us Component finalised with responsiveness

--- src/components/ConnectUs/ConnectUs.jsx
+++ src/components/ConnectUs/ConnectUs.jsx
@@ -1,20 +1,50 @@
 import React from 'react'
-import { Row, Col, Layout } from 'antd'
+import { Row, Col, Layout, Button } from 'antd'
+import Border from './../../assests/images/Rectangle 5 (1).png'
+import LinkedInLogo from '../../assests/images/LinkedIn 2.png'
+import FacebookLogo from '../../assests/images/Facebook F.png'
+import TwitterLogo from '../../assests/images/Twitter.png'
+import InstagramLogo from '../../assests/images/Instagram.png'
+import './ConnectUs.css'
 
 const { Content } = Layout;
 function ConnectUs() {
 
   return (
-    <Layout>
+    <Layout className='body'>
                <Content >
         <Row> 
                 
-                <Col span={24} style ={{backgroundColor: '#0D1625'}}>
-                <div style={{textAlign: 'center', color: '#61C2D0', borderBottom: '2px solid #61C2D0 '}}>
-                     <h2>CONNECT WITH US</h2>
+                <Col span={24}>
+                <div className='main-heading'>
+                     <h2 >
+                      <span>CONNECT WITH US </span></h2>
+                      <img src={Border} alt='border'></img>
                 </div>
                 </Col>
                  
+      </Row>
+      <Row>
+        <Col span={24} className ='column-social-icon' >
+          <a href ='https://www.facebook.com' target='_blank' rel="noopener noreferrer" className='social-link-anchor'>
+          <img src={FacebookLogo} alt= 'facebook'className='image' ></img>
+          </a>
+          <a href ='https://www.instagram.com' target='_blank' rel="noopener noreferrer" className='social-link-anchor'>
+          <img src={InstagramLogo} alt='instagram'className='image' ></img>
+          </a>
+          
+          <a href ='https://www.twitter.com' target='_blank' rel="noopener noreferrer" className='social-link-anchor'>
+          <img src={TwitterLogo} alt='twitter' className='image' ></img>
+          </a>
+          <a href ='https://www.linkedin.com' target='_blank' rel="noopener noreferrer" className='social-link-anchor-linkedIn '>
+          <img src= {LinkedInLogo} className='image' alt= 'linkedIn'></img>
+          </a>
+          </Col>
+      </Row>
+      <Row>
+        <Col span={24} className='button-col' >
+        <Button className='button'>CONTACT US</Button>
+        </Col>
       </Row>
       </Content>
                 </Layout>
