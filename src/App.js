9fc47d290c5eedfe976f43c870629a1ed8fcad3d
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-01 15:49:15 +0500

    testimonials branch testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Test from './components/Test/Test';
+import Testimonials from './components/Test/Testimonials/Testimonials';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={Testimonials}/>
       </Switch>
     </div>
   );
---
+++ src/components/Test/Testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Testimonials.css'
+
+
+function Testimonials() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Testimonials
