2005cfe84585634bccf47fd2656554401883c99e
Anas Khan <anas.raheem2003@gmail.com>
2023-03-02 16:52:48 +0500

    responsive

--- src/components/aboutUsSection/aboutUsSection.js
+++ src/components/aboutUsSection/aboutUsSection.js
@@ -5,22 +5,6 @@ import './aboutUsSection.css';
 
 
 const aboutUsSection = ({ backgroundColor, heading, text }) => {
-    const headingStyle = {
-        fontFamily: "Product Sans",
-        fontSize: "48px",
-        fontWeight: "700",
-        lineWeight: "58px",
-        letterSpacing: "0em"
-      }
-    const paraStyling ={
-        fontFamily: "Product Sans",
-        fontSize: "32px",
-        fontWeight: "400",
-        lineHeight: "51px",
-        letterSpacing: "0em",
-        textAlign: "center"
-
-    }
 
   return (
     <div className="section" >
@@ -29,13 +13,13 @@ const aboutUsSection = ({ backgroundColor, heading, text }) => {
                 <h2 style={{}}>Our Company</h2>
             </div>
             <div className='paragraph'>
-            <p style={{paraStyling}}>NexaLab is a business that creates, develops, and sells software products or services. We specialize in developing software applications, systems, and platforms for a wide variety of purposes, including business, entertainment, education, healthcare, and more.</p>
+            <p style={{}}>NexaLab is a business that creates, develops, and sells software products or services. We specialize in developing software applications, systems, and platforms for a wide variety of purposes, including business, entertainment, education, healthcare, and more.</p>
 
             </div>
         </div>
         <div className='bottom-part'>
             <div>
-                <img src={logo} alt="nexalab logo"></img>
+                <img className="logo" style={{}}src={logo} alt="nexalab logo"></img>
             </div>
         </div>
     </div>
