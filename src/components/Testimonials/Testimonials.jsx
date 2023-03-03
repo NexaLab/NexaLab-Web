f9462840d3c58020eb9224db9328c5af72608e4a
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-03 14:37:06 +0500

    testimonials component completed without responsiveness

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -5,22 +5,27 @@ import Border from '../../assests/images/Rectangle 5 (1).png'
 import Rectangle from '../../assests/images/Group 7.png'
 import Apostrophe from '../../assests/images/“.png'
 import { testinomials } from '../../ProjectData/ProjectData'
+import Logo from '../../assests/images/N Logo.png'
 const {Content} = Layout
 
 function Testimonials() {
   return (
     <Layout className='main'>
-        <Content>
+        <Content >
             
       <Row className='row' > 
-                <Col span={12} className='col' >
+      <Col span={8}>
+            <img src={Rectangle} className = 'rectangle-img' alt ='rectangle'></img>
+        </Col>
+                <Col span={8} className='col' >
                 <div className='testimonial-heading'>
                 <span className='span'>Our Client Love Us
                 <h2 className='h2'>Client Testimonials</h2>
                 </span>                
                 <img className='border-img' src={Border} alt='border'></img>
                 </div>
-                <Carousel autoplay autoplaySpeed ={10000}  className='carousel'  >
+                
+                <Carousel autoplay  className='carousel'  >
                 {testinomials.map((testinomial,index)=> (
                   <div >
             <img key={index} className='carousel-img' src={Apostrophe} alt="Aposhtrophe"></img>
@@ -28,18 +33,13 @@ function Testimonials() {
                    <h3 className='carousel-h3'>{testinomial.name} - {testinomial.Country}</h3>
                 </div>
   
-  ))}
-    
+  ))} 
   </Carousel>
                 </Col> 
-                <Col span={12}>
-            <img src={Rectangle} className = 'rectangle-img' alt ='rectangle'></img>
+                <Col span={8}>
+            <img src={Logo} style ={{width: '70%', marginRight: '-135px', marginTop: '76%', marginBottom:'-30px'}} alt ='logo'></img>
         </Col>
       </Row>
-      <Row>
-      
-      </Row>
-      
       </Content>
       </Layout>
 
