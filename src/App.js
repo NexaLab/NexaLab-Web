e3af5bdb1dc2d7fa325fbfa289a3017c7dffd24c
Afshal Hassan <afshal.hassan@venturedive.com>
2023-03-04 01:48:27 +0500

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Test from './components/Test/Test';
+import Home from './pages/Home';
+
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={Home}/>
       </Switch>
     </div>
   );
