2036209535d889e073061db2d11a8fb492cd36f1
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-03 12:09:46 +0500

    Our services testimonial component check

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Test from './components/Test/Test';
+import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={Testimonials}/>
       </Switch>
     </div>
   );
---
+++ src/ProjectData/ProjectData.js
@@ -0,0 +1,34 @@
+export const testinomials = [
+    {   id: 1,
+        paragraph: "Project delivered on time and also explained us how the project works on live meeting. NexaLab has the best developers to work with. Thank you NexaLab for your help on my Spring boot and React project.",
+        name: "etechwebschool",
+        Country: "USA"
+    },
+    {   
+        id: 2,
+        paragraph: "Excellent job. We have worked with many companies but by far NexaLab is a company that is always available to the client and does all the things flawlessly. We will do the next projects again with NexaLab.",
+        name: "Ahmad Khan",
+        Country: "Sweden"
+    }
+    ,
+    {
+        id: 3,
+        paragraph: "Awesome work great company and really open for RND. Great service. These guys are wondrous. They met all our deadlines and were quick to respond to all our questions.",
+        name: "Vade Singh",
+        Country: "India"
+    },
+    {
+        id: 4,
+        paragraph: "NexaLab has the best team who understands your needs. What sets them apart is that they show up and explain the project after every milestone. Will definitely look for them again.",
+        name: "Pandaxangel",
+        Country: "Singapore"
+    },
+    {
+        id: 5,
+        paragraph: "NexaLab was helpful during and also after the project. Highly recommend for developing CRUD applications.",
+        name: "Shreenath",
+        Country: "India"
+
+    }
+    
+]
