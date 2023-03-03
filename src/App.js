ed063382dc9053ae5a11746083ea2d324f92b0fe
Anas Khan <anas.raheem2003@gmail.com>
2023-03-03 21:43:45 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Test from './components/Test/Test';
+import ourServicesSection from './components/ourServicesSection/ourServicesSection';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={ourServicesSection}/>
       </Switch>
     </div>
   );
---
+++ src/components/ourServicesSection/ourServicesSection.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './ourServicesSection.css'
+
+function ourServicesSection() {
+    return (
+        <div className='section'>
+            <div className='container'>
+                <div className='paragraph'>
+                    <div className='heading'>
+
+                    </div>
+                    <div className='content'>
+
+                    </div>
+                </div>
+                <div className='image'>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ourServicesSection;
