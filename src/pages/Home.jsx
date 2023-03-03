aa1af3136217e180b11a64ba13e146ace2309607
Afshal Hassan <afshal.hassan@venturedive.com>
2023-03-04 02:22:25 +0500

    Home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import "./Home.css";
 import Navbar from "../components/Navbar/Navbar";
 import OurService from "../components/Our Service/OurService";
 import OurTeam from "../components/Our Team/OurTeam";
+import ConnectUs from "../components/ConnectUs/ConnectUs";
+
+
+
 
 function Home() {
   return (
@@ -10,6 +14,7 @@ function Home() {
         <Navbar/>
         <OurService/>
         <OurTeam/>
+        <ConnectUs/>
     </>
   )
 }
