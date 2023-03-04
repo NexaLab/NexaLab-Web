ff8fe47632be90d380f2f658deb0ba753aa59fb3
Afshal Hassan <afshal.hassan@venturedive.com>
2023-03-04 19:16:44 +0500

    About Us Hero component completed with responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Test from './components/Test/Test';
+import AboutUsHero from './components/AboutUsHero/AboutUsHero';
+
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={AboutUsHero}/>
       </Switch>
     </div>
   );
