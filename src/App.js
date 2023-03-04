b6e7596a13993fc2980575a5ab176ae923f7509c
Aneeque-Junaid <aneequej2002@gmail.com>
2023-03-04 14:32:32 +0500

    Message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Test from './components/Test/Test';
+import AboutusServices from './components/Aboutus-Services/AboutusServices';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={AboutusServices} />
       </Switch>
     </div>
   );
