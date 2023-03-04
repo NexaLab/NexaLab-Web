b4c1fb220c0036b553f7b49e20558870269710cf
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-05 02:13:11 +0500

    about us third section completed with responsiveness

---
+++ src/ProjectData/ProjectData.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        image: 'https://i.ibb.co/Kmw6dXt/Profile.png',
+        heading: 'Personal Immersion',
+        paragraph: 'Every client is important. Our team provides personal attention to the details.'
+    },
+    {
+        image: 'https://i.ibb.co/hysbgDs/Facebook-Like.png',
+        heading: 'Customer Satisfaction',
+        paragraph: 'If you are not satisfied. We are not done. We make sure that you leave with a smile.'
+    },
+    {
+        image: 'https://i.ibb.co/p3p6CNr/Money-Bag.png',
+        heading: 'Charges?',
+        paragraph: "Don't worry about the charges. Discuss the project. We will offer a market competitive price."
+    },
+    {
+        image: 'https://i.ibb.co/M6jw0fd/Approval.png',
+        heading: 'Quality Assurance',
+        paragraph: "Our quality assurance department makes sure that every project is duly tested before the deployment."
+    }
+]
--- src/components/ThirdSection/ThirdSection.jsx
+++ src/components/ThirdSection/ThirdSection.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Layout, Row } from 'antd'
+import {data} from '../../ProjectData/ProjectData'
 import './ThirdSection.css'
-import { Layout } from 'antd'
 
 const {Content} = Layout;
 
 function ThirdSection() {
+  const [ deviceWidth, setDeviceWidth] = useState(window.innerWidth);
+
+  const calculateDeviceWidth = () => {
+    setDeviceWidth(window.innerWidth);
+  }
+
+
+  useEffect( () => {
+
+    window.addEventListener('resize', calculateDeviceWidth);
+
+    return () => {
+      window.removeEventListener('resize', calculateDeviceWidth);
+    };
+
+  },[]);
+
   return (
-    <Layout>
-        <Content>
-      <h1>Hello third section</h1>
-      </Content>
-      </Layout>
+      <Layout className='main'>
+            <Content >
+            <Row>
+              {data.map((col,index) => (
+                <Col className='col' span={deviceWidth > 1199 ? 6 : 24}>
+                  <img key={index} src={col.image} alt ='1' style={{width: '25%'}}></img>
+                  <h3>{col.heading}</h3>
+                  <p className='para'>{col.paragraph}</p>
+                </Col>
+              ))}
+            </Row>
+          </Content>
+        </Layout>
   )
 }
 
