34c6ea2922ea0a261cff25c4c592469af57dcd50
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-04 17:04:29 +0500

    about us page third section test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Test from './components/Test/Test';
+import ThirdSection from './components/ThirdSection/ThirdSection';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Test}/>
+        <Route exact path="/" component={ThirdSection}/>
       </Switch>
     </div>
   );
---
+++ src/components/ThirdSection/ThirdSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ThirdSection.css'
+import { Layout } from 'antd'
+
+const {Content} = Layout;
+
+function ThirdSection() {
+  return (
+    <Layout>
+        <Content>
+      <h1>Hello third section</h1>
+      </Content>
+      </Layout>
+  )
+}
+
+export default ThirdSection
