24c34805fa610ad7d68c2aef5ba66f7b7b8333fe
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-07 22:43:35 +0500

    our service second section created

---
+++ src/components/OurServiceSecondSection/OurServiceSecondSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './OurServiceSecondSection.css'
+
+
+function OurServiceSecondSection() {
+  return (
+    <div>
+      <h1>Helllooo</h1>
+    </div>
+  )
+}
+
+export default OurServiceSecondSection
