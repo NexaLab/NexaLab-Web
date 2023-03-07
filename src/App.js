a7b3bc154b7a0164e18c017cab6da50619e65a4e
Afshal Hassan <afshal.hassan@venturedive.com>
2023-03-07 07:24:45 +0500

    About us page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import ThirdSection from './components/ThirdSection/ThirdSection';
-=======
 import Hero from '././components/Hero/Hero';
 import Home from './pages/Home';
 
 
 
->>>>>>> about-us-page
 
 function App() {
   return (
     <div className="App">
       <Switch>
-<<<<<<< HEAD
-        <Route exact path="/" component={ThirdSection}/>
-=======
         <Route exact path="/" component={Home}/>
->>>>>>> about-us-page
       </Switch>
     </div>
   );
